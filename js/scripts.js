dd46095603af36fd12a6e713f509fb7643d47920
hmcvay <hayleymcvay8@gmail.com>
2022-03-31 09:05:42 -0700

    Add resume image to resume section, and project images to projects.

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,29 @@ $(document).ready(function() {
     $(".resume").hide();
     $(".about").show();
   })
-})
+
+  $("#projects").click(function() {
+    $(".splash-main").hide();
+    $(".about").hide();
+    $(".other").hide();
+    $(".resume").hide();
+    $(".projects").show();
+  })
+
+  $("#other").click(function() {
+    $(".splash-main").hide();
+    $(".about").hide();
+    $(".projects").hide();
+    $(".resume").hide();
+    $(".other").show();
+  })
+
+  $("#resume").click(function() {
+    $(".splash-main").hide();
+    $(".about").hide();
+    $(".projects").hide();
+    $(".other").hide();
+    $(".resume").show();
+  })
+
+});
